3d7bcac669dc5adbbf344731a4b56ea92ac3cbf9
Leonid Sulin <sulin.leonid@gmail.com>
2023-06-08 10:01:57 +0300

    Add Game model

--- backend/portal/admin.py
+++ backend/portal/admin.py
@@ -1,9 +1,23 @@
 from django.contrib import admin
 from .models import Tag
+from .models import Game
+from .models import GameTag
+
 
 class TagAdmin(admin.ModelAdmin):
     list_display = ('id', 'label')
 
+
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'releaseYear', 'publisher', 'logoUrl')
+
+
+class GameTagAdmin(admin.ModelAdmin):
+    list_display = ('game', 'tag')
+
+
 # Register your models here.
 
 admin.site.register(Tag, TagAdmin)
+admin.site.register(Game, GameAdmin)
+admin.site.register(GameTag, GameTagAdmin)
--- backend/portal/models.py
+++ backend/portal/models.py
@@ -2,6 +2,20 @@ from django.db import models
 
 # Create your models here.
 
+
 class Tag(models.Model):
     id = models.IntegerField(primary_key=True)
     label = models.CharField(max_length=120)
+
+
+class Game(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=120)
+    releaseYear = models.IntegerField()
+    publisher = models.CharField(max_length=120)
+    logoUrl = models.CharField(max_length=480)
+
+
+class GameTag(models.Model):
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
